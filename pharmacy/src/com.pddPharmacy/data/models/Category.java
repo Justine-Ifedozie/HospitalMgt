ade1208b46ecf5b12b4918c0238ac30bf8b09f47
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-06 06:38:41 +0100

    Working on Pharmacy system with pure java using layered architecture.

--- pharmacy/src/com.pddPharmacy/data/models/Category.java
+++ pharmacy/src/com.pddPharmacy/data/models/Category.java
@@ -1,5 +1,19 @@
 package data.models;
 
 public enum Category {
-    ANALGESIC, ANTIBIOTIC, SUPPLEMENT, MULTIVITAMIN
+    SYRUP(1), TABLET(10), CAPSULE(5), IV(2);
+
+    private int numberInPack;
+
+    Category(int numberAdministered) {
+        this.numberInPack = numberAdministered;
+    }
+
+    public int getNumberInPack() {
+        return numberInPack;
+    }
+
+    public void setNumberInPack(int numberInPack) {
+        this.numberInPack = numberInPack;
+    }
 }
