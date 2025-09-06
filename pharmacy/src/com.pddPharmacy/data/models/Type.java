8f3a407bb5f0734ee52d557347c81049782912d2
Justine-Ifedozie <ifedoziennamdi445@gmail.com>
2025-09-06 06:43:16 +0100

    I updated the Type Enum class

--- pharmacy/src/com.pddPharmacy/data/models/Type.java
+++ pharmacy/src/com.pddPharmacy/data/models/Type.java
@@ -1,25 +1,5 @@
 package data.models;
 
 public enum Type {
-    TABLET, CAPSULE, SYRUP(numberInAPack:1);
-
-    private int numberInAPack;
-    Type(int numberInAPack) {
-        this.numberInAPack = numberInAPack;
-    }
-
-    public int getNumberInAPack() {
-        return numberInAPack;
-    }
-
-    public void  setNumberInAPack(int numberInAPack) {
-        this.numberInAPack = numberInAPack;
-    }
-
-    @Override
-    public String toString() {
-        return "Type{" +
-                "numberInAPack=" + numberInAPack +
-                '}';
-    }
+    ANTIBIOTICS, ANALGESIC, MULTIVITAMIN, ANTIFUNGAL, PROBIOTICS
 }
